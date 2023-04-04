ef3262e45824f5925e79004cbd3bba4864533676
Nils Oberg <noberg@illinois.edu>
2023-04-04 14:53:02 -0500

    Hacky approach to creating efi.config file

---
+++ make_efi_config.pl
@@ -0,0 +1,49 @@
+#!/bin/env perl
+
+use strict;
+use warnings;
+
+
+my ($outputFile, $dbName);
+my $output = GetOptions(
+    "output=s"          => \$outputFile,
+    "db-name=s"         => \$dbName,
+);
+
+die "Need --output output file" if not $outputFile or not -f $outputFile;
+
+$dbName = "" if not $dbName;
+
+
+open my $fh, ">", $outputFile or die "Unable to write to $outputFile: $!";
+
+$fh->print(<<CONFIG);
+[database]
+user=
+password=
+;host=
+;port=
+;ip_range=
+;database=$dbName
+
+[idmapping]
+table_name=idmapping
+uniprot_id=uniprot_id
+
+[idmapping.maps]
+GI=enabled
+EMBL-CDS=enabled
+RefSeq=enabled
+PDB=enabled
+
+[cluster]
+queue=
+extra_path=
+
+CONFIG
+;
+
+close $fh;
+
+
+
